2ff79c907255d623858b574dfd7046fd7e43600e
MuyiwaAbim <oabimbola123@gmail.com>
2022-08-04 00:51:06 -0400

    Updated

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main
         for(int i = 5; i >= 1; i--)
         {
 
-            for(int j = 1; j <= i; j++)
+            for(int j = 5; j >= i; j--)
             {
                 System.out.print("*");
             }
